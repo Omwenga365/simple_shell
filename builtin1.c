ce39b8706d1a89466082bb15e6e30d2918d573c7
Omwenga365 <victoromwenga365@gmail.com>
2023-11-12 22:48:10 -0800

    updated builtin.c

--- builtin1.c
+++ builtin1.c
@@ -1,12 +1,5 @@
 #include "shell.h"
 
-/**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
 int _myhistory(info_t *info)
 {
 	print_list(info->history);
@@ -15,9 +8,6 @@ int _myhistory(info_t *info)
 
 /**
  * unset_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- *
  * Return: Always 0 on success, 1 on error
  */
 int unset_alias(info_t *info, char *str)
@@ -33,16 +23,7 @@ int unset_alias(info_t *info, char *str)
 	ret = delete_node_at_index(&(info->alias),
 		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
 	*p = c;
-	return (ret);
-}
-
-/**
- * set_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- *
- * Return: Always 0 on success, 1 on error
- */
+	return (ret)
 int set_alias(info_t *info, char *str)
 {
 	char *p;
@@ -56,13 +37,6 @@ int set_alias(info_t *info, char *str)
 	unset_alias(info, str);
 	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
-
-/**
- * print_alias - prints an alias string
- * @node: the alias node
- *
- * Return: Always 0 on success, 1 on error
- */
 int print_alias(list_t *node)
 {
 	char *p = NULL, *a = NULL;
@@ -79,13 +53,6 @@ int print_alias(list_t *node)
 	}
 	return (1);
 }
-
-/**
- * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
 int _myalias(info_t *info)
 {
 	int i = 0;
